2816caf61a31dcf32711dae12a42244b70b0b608
sigmajove <sigmajove@gmail.com>
2024-05-08 04:16:57 -0700

    A whole bunch of changes

--- scripts.js
+++ scripts.js
@@ -1,161 +1,3 @@
-// The strategy table.
-let goodMoves = new Map();
-
-// Rotates a board string 90 degrees counterclockwise.
-function rotate(b) {
-    return `${b[2]}${b[5]}${b[8]}${b[1]}${b[4]}${b[7]}${b[0]}${b[3]}${b[6]}`;
-
-}
-
-// Returns the mirror image of a board string.
-function mirror(b) {
-    return `${b[2]}${b[1]}${b[0]}${b[5]}${b[4]}${b[3]}${b[8]}${b[7]}${b[6]}`;
-}
-
-// Calls func on the eight symmetric variations on board.
-function symmetry(func, board) {
-    let b = board;
-    rotate3 = () => {
-        for (let i = 0; i < 3; ++i) {
-            func(b);
-            b = rotate(b);
-        };
-    }
-    rotate3();
-    func(b);
-    b = mirror(b);
-    rotate3();
-    func(b);
-}
-
-// Adds the moves dictated by an one of the strategy patterns.
-function addGoodMoves(board) {
-    let moves = [];
-    let boardArr = board.split("");
-
-    for (let i = 0; i < boardArr.length; ++i) {
-        if (boardArr[i] == "$") {
-            moves.push(i);
-            boardArr[i] = " ";
-        }
-    }
-    goodMoves.set(boardArr.join(""), moves);
-}               
-
-{
-    // Each string represents the nine squares 012345678
-    // Each $ represents an acceptable next move.
-    // Each good move has eight reflections and rotations.
-    // If a board position is not listed, then any move is acceptable.
-    const patterns = [
-        "X   $    ", "X   $O X ", "X  $$X$ O", "X $   $ O", "X $ $O$  ",
-        "X $ X $ O", "X O    X$", "X O$  $ $", "X O$ X  O", "X O$$X   ",
-        "X O$$X O ", "X O$$X XO", "X$ $O$ $X", "X$$ OX $$", "X$$ OX$$O",
-        "XO    $X$", "XO   O$X$", "XO  $X   ", "XO  $XO $", "XO  $XOX$",
-        "XO  O $X ", "XO  OX$X$", "XO $ X  O", "XO $$ $  ", "XO $$X$XO",
-        "XO $X $ O", "XO O X  $", "XO O X X$", "XO OOX X$", "XO X$ O  ",
-        "XO X$$OX$", "XOO$ $$X$", "XOO$ X XO", "XOO$$X$$ ", "XOOO X X$",
-        "XOX   O $", "XOX $    ", "XOX $$OX$", "XOXO  OX$", "XOXO$   $",
-        "XOXO$  X$", "XOXX  O$ ", "XXO   $O$", "XXO  $  $", "XXO  X$ O",
-        "XXO  X$O ", "OX   X$  ", "OX $$ $ $", "OX $$X$O$", "OX $X$$O$",
-        "OX X$$ $ ", "OX$X$O $$", "OXOX$    ", " X OXX$O ", " X$O X  $",
-        " X$O X O ", " X$OOX   ", "$ $ X $ $", "$O$$X$$ $", "$X    O  ",
-        "$X   XO $", "$X O$    ", "$X OOX$X ", "$X$ $  $ ", "$X$ X $O$",
-        "$X$$O$$ $", "$X$OX  O ", "$X$XO $  " ];
-    for (let i = 0; i < patterns.length; ++i) {
-        symmetry((b) => { addGoodMoves(b); }, patterns[i]);
-    }
-}
-
-// Calls func on horizontal, vertical, and diagonal rows.
-function checkRows(func) {
-    func(0, 1, 2);
-    func(3, 4, 5);
-    func(6, 7, 8);
-    func(0, 3, 6);
-    func(1, 4, 7);
-    func(2, 5, 8);
-    func(0, 4, 8);
-    func(2, 4, 6);
-}
-
-// If a row contains two whos in a row and a space, returns
-// the index of the space. Returns every such index, may be empty.
-function checkTwoInARow(board, who) {
-    let moves = []
-    checkRows((x, y, z) => {
-        let bx = board[x];
-        let by = board[y];
-        let bz = board[z];
-        if (bx == " " && by == who && bz == who) {
-            moves.push(x);
-        } else if (by == " " && bx == who && bz == who) {
-            moves.push(y);
-        } else if (bz == " " && bx == who && by == who) {
-            moves.push(z);
-        }});
-    return moves;
-}
-
-// Returns all the possible good computer moves from this position.
-function getComputerMove(board) {
-    let count = 0;
-    board.forEach((p) => {if (p != " ") {++count};});
-    if (count % 2 == 0) {
-        var me = "X";
-        var him = "O";
-    } else {
-        var me = "O";
-        var him = "X";
-    }
-
-    // See if I can win right now.
-    let moves = checkTwoInARow(board, me);
-
-    // If not, see if it needs to block.
-    if (moves.length == 0) {
-        moves = checkTwoInARow(board, him);
-    }
-
-    // If not, see if the board is in GoodMoves.
-    if (moves.length == 0) {
-        moves = goodMoves.get(board.join(""));
-    }
-
-    // If not, just pick any empty square.
-    if (moves === undefined) {
-        moves = [];
-        for (let i = 0; i < 9; ++i) {
-            if (board[i] == " ") {
-                moves.push(i);
-            }
-        }
-    }
-
-    // Return one of the moves at random, or null if there is none.
-    // There should always be one. 
-    return moves.length == 0 ? null :
-        moves[Math.floor(Math.random() * moves.length)];
-}
-
-function hasWinner(board) {
-    let winner = false;
-    checkRows((x, y, z) => {
-        let bx = board[x];
-        let by = board[y];
-        let bz = board[z];
-        if (bx == by && by == bz && bx != " ") {
-            winner = true;
-        }});
-    return winner;
-}
-
-function boardFull(board) {
-    let count = 0;
-    board.forEach((p) => {if (p == " ") {++count};});
-    return count == 0;
-}
-
 function squareButton(i) { return `<button onclick="clickSquare(${i})"
 style="display:block; background: transparent; border: none !important;
 font-size:0; height:50px; width:50px;">
@@ -178,19 +20,6 @@ style="fill:none; stroke:blue; stroke-width:4" />
 
 // Displayed when the game is over.
 const restartHTML = `<button onclick="restartButton()">Play Again</button>`;
-
-// Array representation of the board. Values are "X", "O", and " ".
-// It is kept consistent with the cell elements in the DOM.
-let board = null;
-
-let gameOver = null;
-
-// Used to disable buttons during animation.
-let working = false;
-
-// Which players are which. The values are "X" or "O".
-let myXO = null;
-let computerX0 = null;
 
 // A message area at the bottom of the screen.
 const message = document.getElementById("message");
@@ -234,22 +63,34 @@ let cells = []
 // Function invoked by the initial dialog. It initializes and starts the game.
 function playButton() {
     switch (document.querySelector("input[name=who]:checked").value) {
+        // Record the human and computer's pieces.
         case "first":
-            myXO = "X";
-            computerXO = "O";
+            globalThis.humanXO = "X";
+            globalThis.computerXO = "O";
             break;
         case "second":
-            myXO = "O";
-            computerXO = "X";
+            globalThis.humanXO = "O";
+            globalThis.computerXO = "X";
+            break;
+    }
+    switch (document.querySelector("input[name=difficulty]:checked").value) {
+        case "beatable":
+            globalThis.beatable = true;
+            break;
+        case "unbeatable":
+            globalThis.beatable = false;
             break;
     }
 
-    gameOver = false;
-    board = Array.from(" ".repeat(9));
-    if (myXO == "O") {
-        // It does not matter where the first move goes.
-        board[Math.floor(Math.random() * 9)] = "X";
-    }
+    // Array representation of the board. Values are "X", "O", and " ".
+    // It is kept consistent with the cell elements in the DOM.
+    globalThis.board = Array.from(" ".repeat(9));
+
+    globalThis.gameOver = false;
+
+    // Used to disable buttons during animation.
+    globalThis.working = false;
+
     message.innerHTML = "<p>Click on a square to make a move</p>";
 
     // Turn off the selector and turn on the board and message.
@@ -258,14 +99,17 @@ function playButton() {
     message.style.display = "block";
 
     // Initialize the board.
-    for (let i = 0; i < cells.length; ++i) {
-        cells[i].innerHTML = board[i] == " " ? squareButton(i) : xSquare;
-    }
+    cells.forEach((val, i, a) =>
+        a[i].innerHTML = board[i] == " " ? squareButton(i) : xSquare);
 
     // Add a warning on reload, since that will restart the game.
     // The value of the string returned does not appear to matter,
-    // and there is no way to change the generic warning message.
+    // and I know no way to change the generic warning message.
     window.onbeforeunload = () => "";
+
+    if (computerXO == "X") {
+        MakeComputerMove();
+    }
 }
 
 // Restarts the game with the player selection dialog.
@@ -273,71 +117,6 @@ function restartButton() {
     document.getElementById("selector").style.display = "block";
     document.getElementById("board").style.display = "none";
     message.style.display = "none";
-}
-
-// Rotates a board string 90 degrees counterclockwise.
-function rotate(b) {
-    return `${b[2]}${b[5]}${b[8]}${b[1]}${b[4]}${b[7]}${b[0]}${b[3]}${b[6]}`;
-
-}
-// Returns the mirror image of a board string.
-function mirror(b) {
-    return `${b[2]}${b[1]}${b[0]}${b[5]}${b[4]}${b[3]}${b[8]}${b[7]}${b[6]}`;
-}
-
-// Calls func on the eight symmetric variations on board.
-function symmetry(func, board) {
-    let b = board;
-    rotate3 = () => {
-        for (let i = 0; i < 3; ++i) {
-            func(b);
-            b = rotate(b);
-        };
-    }
-    rotate3();
-    func(b);
-    b = mirror(b);
-    rotate3();
-    func(b);
-}
-
-// Adds the moves dictated by an one of the strategy patterns.
-function addGoodMoves(board) {
-    let moves = [];
-    let boardArr = board.split("");
-
-    for (let i = 0; i < boardArr.length; ++i) {
-        if (boardArr[i] == "$") {
-            moves.push(i);
-            boardArr[i] = " ";
-        }
-    }
-    goodMoves.set(boardArr.join(""), moves);
-}               
-
-{
-    // Each string represents the nine squares 012345678
-    // Each $ represents an acceptable next move.
-    // Each good move has eight reflections and rotations.
-    // If a board position is not listed, then any move is acceptable.
-    const patterns = [
-        "X   $    ", "X   $O X ", "X  $$X$ O", "X $   $ O", "X $ $O$  ",
-        "X $ X $ O", "X O    X$", "X O$  $ $", "X O$ X  O", "X O$$X   ",
-        "X O$$X O ", "X O$$X XO", "X$ $O$ $X", "X$$ OX $$", "X$$ OX$$O",
-        "XO    $X$", "XO   O$X$", "XO  $X   ", "XO  $XO $", "XO  $XOX$",
-        "XO  O $X ", "XO  OX$X$", "XO $ X  O", "XO $$ $  ", "XO $$X$XO",
-        "XO $X $ O", "XO O X  $", "XO O X X$", "XO OOX X$", "XO X$ O  ",
-        "XO X$$OX$", "XOO$ $$X$", "XOO$ X XO", "XOO$$X$$ ", "XOOO X X$",
-        "XOX   O $", "XOX $    ", "XOX $$OX$", "XOXO  OX$", "XOXO$   $",
-        "XOXO$  X$", "XOXX  O$ ", "XXO   $O$", "XXO  $  $", "XXO  X$ O",
-        "XXO  X$O ", "OX   X$  ", "OX $$ $ $", "OX $$X$O$", "OX $X$$O$",
-        "OX X$$ $ ", "OX$X$O $$", "OXOX$    ", " X OXX$O ", " X$O X  $",
-        " X$O X O ", " X$OOX   ", "$ $ X $ $", "$O$$X$$ $", "$X    O  ",
-        "$X   XO $", "$X O$    ", "$X OOX$X ", "$X$ $  $ ", "$X$ X $O$",
-        "$X$$O$$ $", "$X$OX  O ", "$X$XO $  " ];
-    for (let i = 0; i < patterns.length; ++i) {
-        symmetry((b) => { addGoodMoves(b); }, patterns[i]);
-    }
 }
 
 // Flashes a square to emphasize the computer's move.
@@ -362,33 +141,136 @@ function blinkSquare(id) {
     });
 }
 
+// Calls func on horizontal, vertical, and diagonal rows.
+function checkRows(func) {
+    func(0, 1, 2);
+    func(3, 4, 5);
+    func(6, 7, 8);
+    func(0, 3, 6);
+    func(1, 4, 7);
+    func(2, 5, 8);
+    func(0, 4, 8);
+    func(2, 4, 6);
+}
+
 // Returns true if the game is over.
-function checkGameOver(who) {
-    let winner = false;
+function checkGameOver() {
+    let winner = " ";
     checkRows((x, y, z) => {
         let bx = board[x];
         let by = board[y];
         let bz = board[z];
         if (bx == by && by == bz && bx != " ") {
-            winner = true;
-        }});
-    if (winner) {
-        message.innerHTML = `<h3>${who} wins ${restartHTML}</h3>`;
+            winner = bx;
+        }
+    });
+    if (winner != " ") {
+        message.innerHTML = `<h3>${winner} wins ${restartHTML}</h3>`;
         return true;
     }
-    for (let i = 0; i < 9; ++i) {
-        if (board[i] == " ") {
-            return false;
-        }
+
+    // See if there are any empty squares left.
+    if (board.reduce((accum, current) =>
+            current == " " ? true : accum, false)) {
+        return false;
     }
+
     // The game ended in a draw.
     message.innerHTML = `<h3>Tie game ${restartHTML}</h3>`;
     return true;
 }
 
+// Rotates a board string 90 degrees counterclockwise.
+function rotate(b) {
+    return `${b[2]}${b[5]}${b[8]}${b[1]}${b[4]}${b[7]}${b[0]}${b[3]}${b[6]}`;
+
+}
+
+// Returns the mirror image of a board string.
+function mirror(b) {
+    return `${b[2]}${b[1]}${b[0]}${b[5]}${b[4]}${b[3]}${b[8]}${b[7]}${b[6]}`;
+}
+
+// Calls func on the eight symmetric variations on board.
+function symmetry(func, board) {
+    let b = board;
+    rotate3 = () => {
+        for (let i = 0; i < 3; ++i) {
+            func(b);
+            b = rotate(b);
+        };
+    }
+    rotate3();
+    func(b);
+    b = mirror(b);
+    rotate3();
+    func(b);
+}
+
+// The strategy tables.
+let goodMoves = new Map();
+let badMoves = new Map();
+
+// Adds the moves dictated by an one of the strategy patterns.
+function addMoves(board, table) {
+    let moves = [];
+    let boardArr = board.split('');
+
+    boardArr.forEach((s, i, a) => {
+        if (s == '$') {
+            moves.push(i);
+            a[i] = ' ';
+        }
+    });
+    table.set(boardArr.join(''), moves);
+}
+
+{
+    // Each string represents the nine squares 012345678
+    // Each $ represents an acceptable next move.
+    // Each good move has eight reflections and rotations.
+    // If a board position is not listed, then any move is acceptable.
+    const patterns = [
+        "X   $    ", "X   $O X ", "X  $$X$ O", "X $   $ O", "X $ $O$  ",
+        "X $ X $ O", "X O    X$", "X O$  $ $", "X O$ X  O", "X O$$X   ",
+        "X O$$X O ", "X O$$X XO", "X$ $O$ $X", "X$$ OX $$", "X$$ OX$$O",
+        "XO    $X$", "XO   O$X$", "XO  $X   ", "XO  $XO $", "XO  $XOX$",
+        "XO  O $X ", "XO  OX$X$", "XO $ X  O", "XO $$ $  ", "XO $$X$XO",
+        "XO $X $ O", "XO O X  $", "XO O X X$", "XO OOX X$", "XO X$ O  ",
+        "XO X$$OX$", "XOO$ $$X$", "XOO$ X XO", "XOO$$X$$ ", "XOOO X X$",
+        "XOX   O $", "XOX $    ", "XOX $$OX$", "XOXO  OX$", "XOXO$   $",
+        "XOXO$  X$", "XOXX  O$ ", "XXO   $O$", "XXO  $  $", "XXO  X$ O",
+        "XXO  X$O ", "OX   X$  ", "OX $$ $ $", "OX $$X$O$", "OX $X$$O$",
+        "OX X$$ $ ", "OX$X$O $$", "OXOX$    ", " X OXX$O ", " X$O X  $",
+        " X$O X O ", " X$OOX   ", "$ $ X $ $", "$O$$X$$ $", "$X    O  ",
+        "$X   XO $", "$X O$    ", "$X OOX$X ", "$X$ $  $ ", "$X$ X $O$",
+        "$X$$O$$ $", "$X$OX  O ", "$X$XO $  " ];
+    patterns.forEach((p) => symmetry((b) => addMoves(b, goodMoves), p));
+}
+
+{
+  // These patterns have the same syntax, but represent opportunities
+  // for the program to let the Wookiee win.
+  const patterns = [
+    ' O  X  $ ', ' X $$$ O ', ' X $O$$$$', ' X $X$ O ', ' X O $$ $',
+    ' X OX$ O ', ' X X$O O ', ' X XO$ $$', ' X XOO $ ', '$$$$$$$$$',
+    '$$$$X$$$$', '$X X$O$$ ', '$X$$$$$$$', 'O   $X X$', 'O$   X$$ ',
+    'OX X O$  ', 'OX$  X $$', 'OX$  X O ', 'OX$ X  O ', 'OX$$ $$ $',
+    'OX$X $$$$', 'OXOX $$$ ', 'X    $ $O', 'X    O $ ', 'X   O$ $ ',
+    'X  $OX  O', 'X  $OX$  ', 'X $ O $ X', 'X $$ X$O ', 'X$ $X$ $O',
+    'X$$$ X  O', 'X$$$$$$$$', 'X$O  $ $ ', 'X$O  X  O', 'X$O  X O$',
+    'X$O  X$XO', 'X$O$ X$$$', 'X$O$$$$X ', 'XO $ XO  ', 'XO$  $   ',
+    'XO$  X  O', 'XO$  X XO', 'XO$$ X$ $', 'XO$$ XOX ', 'XO$$$$$X$',
+    'XO$$OX X ', 'XO$O$X$X ', 'XOO  X  $', 'XOO$$X$X$', 'XOX$  O  ',
+    'XOX$  OX ', 'XOX$ $$ $', 'XOXO  $  ', 'XOXO $$X ', 'XXO$  $  ',
+    'XXO$$$ O ', 'XXOO  $$$', 'XXOO X$  '
+  ];
+  patterns.forEach((p) => symmetry((b) => addMoves(b, badMoves), p));
+}
+
 // If a row contains two whos in a row and a space, returns
 // the index of the space. Returns every such index, may be empty.
-function checkTwoInARow(who) {
+function checkTwoInARow(board, who) {
     let moves = []
     checkRows((x, y, z) => {
         let bx = board[x];
@@ -404,35 +286,73 @@ function checkTwoInARow(who) {
     return moves;
 }
 
-// Returns all the possible good computer moves from this position.
-function getComputerMove() {
-    // See if it can win right now.
-    let moves = checkTwoInARow(computerXO);
+// Returns the number of Xs and Os on the board.
+function numberFilled(board) {
+    return board.reduce((accum, current) =>
+        current == " " ? accum : accum + 1, 0);
+}
+
+// Returns all the possible computer moves from this position.
+function getComputerMoves(board) {
+    if (numberFilled(board) % 2 == 0) {
+        var me = 'X';
+        var him = 'O';
+    } else {
+        var me = 'O';
+        var him = 'X';
+    }
+
+    // See if the computer can win right now.
+    let moves = checkTwoInARow(board, me);
 
     // If not, see if it needs to block.
     if (moves.length == 0) {
-        moves = checkTwoInARow(myXO);
+        moves = checkTwoInARow(board, him);
     }
 
-    // If not, see if the board is in GoodMoves.
+    // If not, see if the board is the strategy table(s).
     if (moves.length == 0) {
-        moves = goodMoves.get(board.join(""));
-    }
-
-    // If not, just pick any empty square.
-    if (moves === undefined) {
-        moves = [];
-        for (let i = 0; i < 9; ++i) {
-            if (board[i] == " ") {
-                moves.push(i);
+        let lookupKey = board.join('');
+        if (beatable) {
+            moves = badMoves.get(lookupKey);
+            if (moves === undefined) {
+                moves = goodMoves.get(lookupKey);
             }
+        } else {
+            moves = goodMoves.get(lookupKey);
         }
     }
 
-    // Return one of the moves a random, or null if there is none.
-    // There should always be one. 
-    return moves.length == 0 ? null :
-        moves[Math.floor(Math.random() * moves.length)];
+    // If not, find all the empty squares.
+    if (moves === undefined) {
+        moves = [];
+        board.forEach((s, i) => {
+            if (s == ' ') {
+                moves.push(i);
+            }
+        });
+    }
+    return moves;
+}
+
+function MakeComputerMove() {
+    moves = getComputerMoves(board);
+    if (moves.length > 0) {
+        // Choose one of the moves at random.
+        let m = moves[Math.floor(Math.random() * moves.length)];
+
+        // Write the computer move in both the board and the DOM.
+        board[m] = computerXO;
+        cells[m].innerHTML = computerXO == "X" ? xSquare : oSquare;
+
+        // Flash the computer move, and proceed when the animation is over. 
+        blinkSquare(m).then((x)=> {
+            if (checkGameOver()) {
+                gameOver = true;
+                window.onbeforeunload = undefined;
+            }
+        });
+    }
 }
 
 // Invoked when the user clicks on a square.
@@ -441,28 +361,15 @@ function clickSquare(id) {
         return;
     }
 
-    // Write the player move in both the board and the DOM.
-    board[id] = myXO;
-    cells[id].innerHTML = myXO == "X" ? xSquare : oSquare;
+    // Write the human's move in both the board and the DOM.
+    board[id] = humanXO;
+    cells[id].innerHTML = humanXO == "X" ? xSquare : oSquare;
 
-    if (checkGameOver(myXO)) {
+    if (checkGameOver()) {
         gameOver = true;
         window.onbeforeunload = undefined;
         return;
     }
 
-    m = getComputerMove();
-    if (m !== null) {
-        // Write the computer move in both the board and the DOM.
-        board[m] = computerXO;
-        cells[m].innerHTML = computerXO == "X" ? xSquare : oSquare;
-
-        // Flash the computer move, and proceed when the animation is over. 
-        blinkSquare(m).then((x)=> {
-            if (checkGameOver(computerXO)) {
-                gameOver = true;
-                window.onbeforeunload = undefined;
-            }
-        });
-    }
+    MakeComputerMove();
 }
