1f65f45ff683891db44578288ac1d717a2a9578b
sigmajove <sigmajove@gmail.com>
2024-05-10 04:07:07 -0700

    really big button

---
+++ deploy.bat
@@ -0,0 +1,5 @@
+"C:\Program Files\Minify\minhtml.exe" --minify-js --minify-css index.html ^
+-o C:\Users\scott\OneDrive\Documents\ttt_js\public\index.html
+
+uglifyjs scripts.js ^
+-o C:\Users\scott\OneDrive\Documents\ttt_js\public\scripts.js
